c60692be9e66472b360e09b9d5b3db7fc394d407
VANIERCOLLEGE\2295472 <2295472@edu.vaniercollege.qc.ca>
2023-08-18 13:57:58 -0400

    Redo  names & created sign-up-login-view.spec.js

---
+++ tests/sign-up-login-view.spec.js
@@ -0,0 +1,39 @@
+describe("StreamNow.vue", () => {
+  it("displays the h1 title for login", () => {});
+
+  it("displays the paragraph for login", () => {});
+
+  it("displays the search input for username", () => {});
+
+  it("displays the search input for password", () => {});
+
+  it("displays the button for forgot password", () => {});
+
+  it('emits "forgot-password" event when "Forgot Password" button is clicked', async () => {});
+
+  it("displays the button for login", () => {});
+
+  it('emits "login" event when "Login" button is clicked', async () => {});
+
+  it("displays the h1 title for sign up", () => {});
+
+  it("displays the paragraph for sign up", () => {});
+
+  it("displays the search input for name", () => {});
+
+  it("displays the search input for email", () => {});
+
+  it("displays the search input for username", () => {});
+
+  it("displays the search input for password", () => {});
+
+  it("displays the search input for confirm password", () => {});
+
+  it("displays the button for sign up", () => {});
+
+  it('emits "sign-up" event when "Sign Up" button is clicked', async () => {});
+
+  it("displays the error message for each individual input fields", () => {});
+
+  it("displays the a confirmation message for successful login or successful sign-up", () => {});
+});
--- tests/unit/add-watch-to-list-view.spec.js
+++ tests/unit/add-watch-to-list-view.spec.js
@@ -5,23 +5,23 @@ describe("AddWatchList.vue", () => {
 
   it('displays input elements for "Notes", "Watch list dropdown elements"', () => {});
 
-  it("updates notes data on input change", async () => { });
-  
+  it("updates notes data on input change", async () => {});
+
   it('displays a "Watchlist" dropdown menu: 1-Watchlist, 2-Create a list, 2.a)list name input, 2.b) "Save list" Button', () => {});
 
   it('updates "Add to Watch list" data on dropdown change', async () => {});
 
-  it('displays a "Clear" button', () => { });
-  
-  it('displays an "Add to watchlist" button', () => {})
+  it('displays a "Clear" button', () => {});
+
+  it('displays an "Add to watchlist" button', () => {});
 
   it('emits "(movie.name) Added to Watch list!" pop-up text event on "Add to Watch list" button click', async () => {});
 
-  it('clears inputs on "Clear" button click', async () => { });
-  
-  it('hides the Add to Watch list button after it is clicked', () => { });
-  
-  it("re-displays the Add to Watch list button if any input elements are updated with button clicks", () => {});
+  it('clears inputs on "Clear" button click', async () => {});
+
+  it("hides the Add to Watch list button after it is clicked", () => {});
+
+  it('re-displays the "Add to Watch list" button if any input elements are updated with button clicks', () => {});
 
   // ... Write tests for the save and clear methods and their behaviors ...
 });
